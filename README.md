74bfd3a6418050be295d99d8731020e731279c9c
richardgourley <codeleap@outlook.com>
2018-07-04 20:17:40 +0200

    JSON file added. Main JS file modified with comments.

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 JSON REST API CALL WITH CLOSURE
 
-This is an example for Javascript intermediate level students on how to create a get request and get information from an api.
+This is an example for Javascript intermediate level students on how to create a get request and get information from a JSON file.
 
-This example shows how you can assign functions with parameters to variables and use (call) these variables later.  This is done using closure.
-Hopefully, when JS students see the power of closure in action, it will help them think about how it could be helpful in their applications!
+You can replace the JSON file in the example with an API endpoint.
+
+In this example we are using a simple JSON file, with data to represent typical objects found in blog posts, with a title, category and a boolean for published - true or false.
+
+An example of closure is added to this example.
+
+ABOUT CLOSURE
+
+- With closure, a variable 'remembers' the environment it was created in.
+- The key point is that we create a variable and add an OUTER function to it.  When we call the variable above we can access the INNER function inside the outer variable!
+- You can pass variables that are assigned functions with closure around your program and they remember the enclosing scope!
+
+- Practice with closure.  The best way is to create an outer() and inner()
+function and play with different scenarios.
+
+- It's very powerful!
+
+- NOTE:  Closure is not exclusive to XMLHttpRequests!  It can be used in many programming situations!  Enjoy!
 
 SKILLS COVERED
 - JSON
--- main.js
+++ main.js
@@ -1,16 +1,58 @@
 
+//This is an outer function which contains an inner function.
+function outer(category){
+    
+    function inner(published){
+        //Inside this inner function...
+        //we assign a new variable to a new XMLHttpRequest
+        var xhr = new XMLHttpRequest();
+        //We set the request to 'GET'
+        //You can change DATA.JSON to an API endpoint as required.
+        xhr.open('GET','data.json', true);
+        xhr.responseType = 'text';
+        xhr.send();
+        
+        //Here we use the onload function for program logic.
+        xhr.onload = function(){
+            //We check the connection is successful
+            if(xhr.status == 200){
+                //We parse JSON to a Javascript object
+                var myObj = JSON.parse(xhr.responseText);
+                //We perform login on the array of objects returned and parsed as 'myObj'
+                for(i=0; i<myObj.length; i++){
+                    if(myObj[i].category == category && myObj[i].published == published){
+                        console.log(myObj[i]);
+                    }
+                }
+            }
+            else{
+                console.log("Sorry, there was a problem.");
+            }
+        }
+    }
+    return inner;
+}
 
-//Here, we pass in a string to create an endpoint.
-//Our function will return an inner function which will return a parsed JSON object...
+//These variables 'remember' the environment they were created in.
+var wordpressCategory = outer("wordpress");
+var javaCategory = outer("java");
+var javascriptCategory = outer("javascript");
 
-var endPointPosts = "posts";
-var endPointCategories = "categories"
+//When we call the function assigned to these variables we access
+//the INNER function!
+javaCategory(false);
+javascriptCategory(false);
+wordpressCategory(true);
 
-//closure involves having an inner and outer function.
-//outer function that takes a string parameter (in our case, posts or categories)
-//inner function that provides api details and then opens XMLHttpRequest.
-//returns the inner function (which returns a parsed Javascript object)
-//these two variables 'remember' the environment they were called in.
-//you can use these variables later in your program.  You can pass them to other functions!
+//You can pass variables, which have been assigned functions with closure, around your program!
+//It's very powerful!
+
+
+
+
+
+
+
+
 
 
